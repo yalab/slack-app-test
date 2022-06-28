cd95c20fde6a6c98cdec4676982dbaacf66471de
yalab <rudeboyjet@gmail.com>
2022-06-28 20:12:39 +0900

    It can handle "hello".

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const { App } = require('@slack/bolt');
 require('dotenv').config()
 
-// Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  port: process.env.PORT || 3000,
+  logLevel: 'debug'
+});
+
+app.message('hello', async ({ message, say }) => {
+  await say(`Hey there <@${message.user}>!`);
 });
 
 (async () => {
-  // Start your app
-  await app.start(process.env.PORT || 3000);
+  await app.start()
 
   console.log('⚡️ Bolt app is running!');
 })();
